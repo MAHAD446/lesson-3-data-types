e7d1254294d1355ab97d620abc2427a14caff373
mahad446 <mahadmubashir43@gmail.com>
2025-01-03 15:47:43 -0800

    activity#3

---
+++ activity3.py
@@ -0,0 +1,10 @@
+#input a world
+text = str(input("enter a string: "))
+
+# reverse string
+# using step value as -1 to iterate in reverse
+revtext = text[:: -1]
+tent = revtext
+
+print("reverse of given strinng is :")
+print(text)
