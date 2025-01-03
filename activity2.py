4d6878c090f6bcf57fef81175f8437c6178606b5
mahad446 <mahadmubashir43@gmail.com>
2025-01-03 15:32:32 -0800

    activity#2

---
+++ activity2.py
@@ -0,0 +1,23 @@
+# assigning different variables
+
+name ="penguin"
+
+age = 15
+
+is_student = True
+
+weight= 38.5
+
+
+#printing differents variabels and their date type
+print("name :", name)
+print("data type of name is", type(name))
+
+print("age :", age)
+print("data tpe of age is ", type(age))
+
+print("is_student:", is_student)
+print("data type of is_stuent is", type(is_student))
+
+print("weight :", weight)
+print("data type of weight is", type(weight))
