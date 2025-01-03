18803f6361f1a7e99691ab01d8617801fb476796
mahad446 <mahadmubashir43@gmail.com>
2025-01-03 15:06:34 -0800

    activity#1

---
+++ activity1.py
@@ -0,0 +1,17 @@
+# let, s check the data type of differen
+a = 5
+
+print("type of a : ", type(a))
+
+
+b=2.5
+print("type of b : " , type(b))
+
+
+c="codingal"
+print("type of c" , type(c))
+
+
+d=True
+print("type of d : " , type(d))
+
